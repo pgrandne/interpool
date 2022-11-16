566a07a896d4900e2e2ac53ab82747167bd69d5a
Perrin <perrin.grandne@protonmail.com>
2022-11-16 23:57:45 +0100

    Update ticket button and add new toast when no goerli

--- client/src/utils/Faucet.tsx
+++ client/src/utils/Faucet.tsx
@@ -1,11 +1,20 @@
-import { useContractWrite, usePrepareContractWrite, useWaitForTransaction } from 'wagmi'
+import { useAccount, useBalance, useContractWrite, usePrepareContractWrite, useWaitForTransaction } from 'wagmi'
 import { ethers } from 'ethers'
 import { goerli } from './contractAddress'
 import { useState } from 'react'
 import { toast } from 'react-toastify';
 
+const GetBalance = () => {
+    const { address } = useAccount()
+    const { data }: { data: any } = useBalance({
+        addressOrName: address,
+    })
+    return (parseFloat(data.formatted))
+}
+
 
 const Faucet = () => {
+    console.log(GetBalance())
     const [loading, setLoading] = useState(false)
     const { config } = usePrepareContractWrite({
         address: goerli.usdcContract,
@@ -23,8 +32,10 @@ const Faucet = () => {
             "type": "function"
         },],
         functionName: 'mint',
-        args: [ethers.utils.parseUnits('100', 6)],
+        args: [ethers.utils.parseUnits('50', 6)],
     })
+
+    const bal: number = GetBalance();
 
     const { data, write } = useContractWrite({
         ...config,
@@ -48,14 +59,25 @@ const Faucet = () => {
         }
     })
 
+    const Msg: any = ({ closeToast, toastProps }: { closeToast: any, toastProps: any }) => (
+        <div>
+            ⚽ You need Goerli ETH to proceed transactions! <br />
+            Go claim < a href="https://goerlifaucet.com/" > here</a > <br />
+            Or ask us on < a href="https://twitter.com/IrruptionLab" > Twitter</a >
+        </div >
+    )
+
     return (
         <a href="/" data-w-id="10726a2a-0f38-c4f5-17d4-b50ee7aa8dd5" className={!loading ? "hollow-button white" : "hollow-button notactive"}
             onClick={(e) => {
                 e.preventDefault()
-                setLoading(true)
-                write?.()
-            }
-            }>{loading && <i className="fa fa-refresh fa-spin"></i>} Faucet 100 USDC</a>
+                if (bal > 0) {
+                    setLoading(true)
+                    write?.()
+                } else {
+                    toast(<Msg />, { autoClose: 10000 })
+                }
+            }}> {loading && <i className="fa fa-refresh fa-spin"></i>} Faucet 50 USDC</a >
     )
 }
 
